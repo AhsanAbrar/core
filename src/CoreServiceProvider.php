ca5ccbd8481bc147c9fb635bb4939fad15776816
Ahsan Abrar <iahsanabrar@gmail.com>
2025-08-20 14:44:47 +0500

    core

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -3,8 +3,8 @@
 namespace Spanvel;
 
 use Illuminate\Support\ServiceProvider;
-// use Spanvel\Http\Middleware\ServePackage;
-// use Illuminate\Contracts\Http\Kernel as HttpKernel;
+use Spanvel\Http\Middleware\ServePackage;
+use Illuminate\Contracts\Http\Kernel as HttpKernel;
 
 class CoreServiceProvider extends ServiceProvider
 {
@@ -21,15 +21,15 @@ class CoreServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // if (! $this->app->configurationIsCached()) {
-        //     $this->mergeConfigFrom(__DIR__.'/../config/span.php', 'span');
-        // }
+        if (! $this->app->configurationIsCached()) {
+            $this->mergeConfigFrom(__DIR__.'/../config/span.php', 'span');
+        }
 
         // if ($this->app->runningInConsole() && ! $this->app->runningUnitTests()) {
         //     Span::registerAllProviders();
         // }
 
-        // $this->app->make(HttpKernel::class)
-        //             ->pushMiddleware(ServePackage::class);
+        $this->app->make(HttpKernel::class)
+                    ->pushMiddleware(ServePackage::class);
     }
 }
---
+++ src/Http/Middleware/ServePackage.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Spanvel\Http\Middleware;
+
+use Spanvel\Span;
+
+class ServePackage
+{
+    /**
+     * The request segment 1.
+     *
+     * @var string
+     */
+    protected $segmentOne;
+
+    /**
+     * The request segment 2.
+     *
+     * @var string
+     */
+    protected $segmentTwo;
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Illuminate\Http\Response
+     */
+    public function handle($request, $next)
+    {
+        $this->setSegments($request);
+
+        if ($provider = $this->isSpanRequest($request)) {
+            app()->register($provider);
+        }
+
+        return $next($request);
+    }
+
+    /**
+     * Set segments for livewire and default request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function setSegments($request)
+    {
+        // i have to refactor this whole block.
+        // waiting for better idea.
+
+        // if ($request->hasHeader('X-Livewire')) {
+            // $path = explode('/', $request->fingerprint['path']);
+            // $this->segmentOne = $path[0] ?? null;
+            // $this->segmentTwo = $path[1] ?? null;
+            // return;
+        // }
+
+        $this->segmentOne = $request->segment(1);
+        $this->segmentTwo = $request->segment(2);
+    }
+
+    /**
+     * Determine if the given request is intended for Span.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    protected function isSpanRequest($request)
+    {
+        // i have to refactor this whole block.
+        // waiting for better idea.
+
+        if (in_array($this->segmentOne, config('span.excluded_routes'))) {
+            return false;
+        }
+
+        $hasPrefix = $this->segmentOne == config('span.prefix');
+        $key = $hasPrefix ? $this->segmentTwo : $this->segmentOne;
+        $providers = $hasPrefix
+            ? config('span.prefix_providers')
+            : config('span.providers');
+
+        if (array_key_exists($key, $providers)) {
+            Span::key($key);
+            Span::prefix($hasPrefix ? config('span.prefix') . '/' . $key : $key);
+
+            return $providers[$key];
+        } elseif (array_key_exists('', $providers)) {
+            Span::prefix($hasPrefix ? config('span.prefix') : '');
+
+            return $providers[''];
+        }
+    }
+}
