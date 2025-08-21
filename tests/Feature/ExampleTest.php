926924068392d98b02b3dfbe148b4088855c6c1a
Ahsan Abrar <iahsanabrar@gmail.com>
2025-08-21 11:21:40 +0500

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,3 +3,7 @@
 test('laravel boots under testbench', function () {
     expect(config('app.env'))->toBe('testing');
 });
+
+test('package service provider boots', function () {
+    expect(app()->providerIsLoaded(\Spanvel\CoreServiceProvider::class))->toBeTrue();
+});
