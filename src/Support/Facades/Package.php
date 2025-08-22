10ff8904af5274a5424945c5b4e7b38622d12883
Ahsan Abrar <iahsanabrar@gmail.com>
2025-08-22 20:46:04 +0500

    Update Package.php

--- src/Support/Facades/Package.php
+++ src/Support/Facades/Package.php
@@ -5,11 +5,13 @@ namespace Spanvel\Support\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static string key(?string $key = null)
+ * @method static \Spanvel\Package\PackageBoot boot(string $packagePath)
+ * @method static string key(string|null $key = null)
+ * @method static void setKey(string $key)
  * @method static void reset()
- * @method static \Spanvel\PackageBoot boot(string $packagePath)
  *
  * @see \Spanvel\Package\PackageContext
+ * @see \Spanvel\Package\PackageBoot
  */
 class Package extends Facade
 {
