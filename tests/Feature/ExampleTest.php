70466853212625a74b4742fbb440b0ab9261da19
Ahsan Abrar <iahsanabrar@gmail.com>
2025-08-22 15:25:02 +0500

    Delete ExampleTest.php

--- tests/Feature/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-test('laravel boots under testbench', function () {
-    expect(config('app.env'))->toBe('testing');
-});
-
-test('package service provider boots', function () {
-    expect(app()->providerIsLoaded(\Spanvel\CoreServiceProvider::class))->toBeTrue();
-});
