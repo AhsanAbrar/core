da3bf307ecc894e0c5fc75b7c8cb10f5cebb738e
Ahsan Abrar <iahsanabrar@gmail.com>
2025-08-22 21:52:39 +0500

    cleanup

---
+++ tests/Feature/PackageBootRoutesOptionsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Tests\Fixtures\Site\src\SiteServiceProvider;
+
+use function Pest\Laravel\get;
+
+describe('PackageBoot routes options', function () {
+    beforeEach(function () {
+        app()->register(SiteServiceProvider::class);
+    });
+
+    it('prepends the package resources/views path', function () {
+        get('/hello')
+            ->assertOk()
+            ->assertSee('From Package View', false);
+    });
+});
