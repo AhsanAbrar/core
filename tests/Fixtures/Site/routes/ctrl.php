8862846c1ca144c4861b7757bd9ee77359d2cb3a
Ahsan Abrar <iahsanabrar@gmail.com>
2025-08-23 11:32:46 +0500

    Delete ctrl.php

--- tests/Fixtures/Site/routes/ctrl.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// This relies on Route::group(['controller' => DemoController::class]) support.
-Route::get('/', 'index');
-Route::get('/about', 'about');
