2f50f64dc0336950bfbccc15d0269d496bb52c65
Ahsan Abrar <iahsanabrar@gmail.com>
2025-08-25 16:42:38 +0500

    basic

--- packages/vue-app/vite.config.ts
+++ packages/vue-app/vite.config.ts
@@ -1,25 +1,30 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import ahsandevs from 'ahsandevs-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
     ahsandevs(),
     vue(),
+    vueDevTools()
   ],
   server: {
     cors: true
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'resources/js'),
-      'appdata': resolve(__dirname, 'resources/js/app-data.ts'),
-      'types': resolve(__dirname, 'resources/js/types/index.d.ts'),
-      'Component': resolve(__dirname, 'resources/js/components'),
-      'Store': resolve(__dirname, 'resources/js/stores'),
-      'Use': resolve(__dirname, 'resources/js/composables'),
-      'View': resolve(__dirname, 'resources/js/views'),
+      '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+      'appdata': fileURLToPath(new URL('./resources/js/app-data.ts', import.meta.url)),
+      'types': fileURLToPath(new URL('./resources/js/types/index.d.ts', import.meta.url)),
+      'Component': fileURLToPath(new URL('./resources/js/components', import.meta.url)),
+      'Store': fileURLToPath(new URL('./resources/js/stores', import.meta.url)),
+      'Use': fileURLToPath(new URL('./resources/js/composables', import.meta.url)),
+      'View': fileURLToPath(new URL('./resources/js/views', import.meta.url)),
     },
   },
   build: {
