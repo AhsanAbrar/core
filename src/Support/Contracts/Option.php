3231b4e4011fa0c890a024732a0f056a7004d5e4
Ahsan Abrar <iahsanabrar@gmail.com>
2025-09-02 16:47:37 +0500

    Update Option.php

--- src/Support/Contracts/Option.php
+++ src/Support/Contracts/Option.php
@@ -7,10 +7,10 @@ interface Option
     /**
      * Get the value of an option by its key.
      */
-    public function get(string $key, string|array $default = null): mixed;
+    public function get(string $key, mixed $default = null): mixed;
 
     /**
      * Create or update an option's value by its key.
      */
-    public function put(string|array $key, string|array $value = null): bool;
+    public function put(string|array $key, mixed $value = null): bool;
 }
