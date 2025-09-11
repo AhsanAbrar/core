9e8514a269f6f880e30c50f32dcfddd2e4d7edad
Ahsan Abrar <iahsanabrar@gmail.com>
2025-09-11 19:33:08 +0500

    Create FactorySingle.php

---
+++ src/Package/FactorySingle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Spanvel\Package;
+
+class FactorySingle
+{
+    protected ?string $basePath = null; // do NOT use this on the singleton
+
+    public function boot(string $basePath): self
+    {
+        $clone = new self; // new, independent instance
+        $clone->basePath = \dirname(\rtrim($basePath, '/\\'));
+        return $clone;
+    }
+
+    public function webRoutes(string $file = 'web.php'): self
+    {
+        $path = $this->basePath;
+
+        dump($path);
+
+        return $this;
+    }
+}
