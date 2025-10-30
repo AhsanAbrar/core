0a644409a72609d3d832fb2f72db85b881aaee88
Ahsan Abrar <iahsanabrar@gmail.com>
2025-10-30 11:47:43 +0500

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,10 +6,21 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 abstract class TestCase extends Orchestra
 {
+    /**
+     * Register package service providers.
+     */
     protected function getPackageProviders($app): array
     {
         return [
             \Spanvel\CoreServiceProvider::class,
         ];
+    }
+
+    /**
+     * Load package migrations.
+     */
+    protected function defineDatabaseMigrations()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
