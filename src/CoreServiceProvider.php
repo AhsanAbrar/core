b71a7b33368e2e7ba450fc51a1efecfc62f54f7e
Ahsan Abrar <iahsanabrar@gmail.com>
2025-11-10 21:18:13 +0500

    Create CoreServiceProvider.php

---
+++ src/CoreServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Spanvel;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
