136903b71680dcbae271e3a9fd7567bb0c386da7
Ahsan Abrar <iahsanabrar@gmail.com>
2025-11-22 22:09:56 +0500

    Update PackageCommand.php

--- src/Console/PackageCommand.php
+++ src/Console/PackageCommand.php
@@ -25,10 +25,28 @@ class PackageCommand extends Command implements PromptsForMissingInput
     protected $description = 'Create a new spanvel package';
 
     /**
+     * Prompt for missing input arguments using the returned questions.
+     */
+    protected function promptForMissingArgumentsUsing(): array
+    {
+        return [
+            'type' => fn () => $this->choice(
+                'Select the package type:',
+                ['basic', 'blade', 'vue', 'app-vue'],
+                'basic'
+            ),
+        ];
+    }
+
+    /**
      * Execute the console command.
      */
     public function handle(): int
     {
+        if ($this->filesystem->isDirectory($destination)) {
+            $this->fail('Spanvel Auth is already installed.');
+        }
+
         $this->info('Spanvel package generated successfully.');
 
         return static::SUCCESS;
