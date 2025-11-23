83477ad315218b3763bb05d7aaf9c47a2a9eae37
Ahsan Abrar <iahsanabrar@gmail.com>
2025-11-23 15:38:02 +0500

    Update Vite.php

--- src/Support/Vite.php
+++ src/Support/Vite.php
@@ -37,6 +37,8 @@ class Vite
         $this->entry = $entry;
 
         if ($this->isRunningHot()) {
+            $this->setHostAndPort();
+
             return $this->renderDevTags();
         }
 
@@ -46,11 +48,40 @@ class Vite
     /**
      * Determine if the HMR server is running.
      */
+    protected function setHostAndPort(): void
+    {
+        $contents = @file_get_contents($this->hotFilePath());
+
+        if ($contents === false) {
+            // If for some reason the file disappeared, keep defaults.
+            return;
+        }
+
+        foreach (preg_split('/\r\n|\r|\n/', $contents) as $line) {
+            if (! str_contains($line, ':')) {
+                continue;
+            }
+
+            [$key, $value] = array_map('trim', explode(':', $line, 2));
+
+            if ($key === 'ip' && $value !== '') {
+                $this->devHost = $value;
+            }
+
+            if ($key === 'port' && is_numeric($value)) {
+                $this->devPort = (int) $value;
+            }
+        }
+    }
+
+    /**
+     * Determine if the HMR server is running.
+     */
     protected function renderDevTags(): HtmlString
     {
         return new HtmlString(
-            sprintf('<script type="module" src="//%s:%s/@vite/client"></script>', $this->ip, $this->port).
-            sprintf('<script type="module" src="//%s:%s/resources/js/%s"></script>', $this->ip, $this->port, $this->file)
+            sprintf('<script type="module" src="//%s:%s/@vite/client"></script>', $this->host, $this->port).
+            sprintf('<script type="module" src="//%s:%s/resources/js/%s"></script>', $this->host, $this->port, $this->entry)
         );
     }
 
@@ -66,10 +97,18 @@ class Vite
     }
 
     /**
+     * Get the path to the hot file for this package.
+     */
+    protected function hotFilePath(): string
+    {
+        return base_path("packages/{$this->package}/hot");
+    }
+
+    /**
      * Determine if the HMR server is running.
      */
     protected function isRunningHot(): bool
     {
-        return file_exists(base_path("packages/{$this->package}/hot"));
+        return is_file($this->hotFilePath());
     }
 }
