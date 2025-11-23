3f9784bdc55206263cebe7d56aa701ab83da32a4
Ahsan Abrar <iahsanabrar@gmail.com>
2025-11-23 15:18:55 +0500

    Update Vite.php

--- src/Support/Vite.php
+++ src/Support/Vite.php
@@ -9,14 +9,45 @@ use Illuminate\Support\HtmlString;
 class Vite
 {
     /**
+     * The name of the package.
+     */
+    protected string $package;
+
+    /**
      * Handle the invocation of the class.
      */
     public function __invoke(string $package, string $entry = 'main.ts'): HtmlString
     {
+        $this->package = $package;
+
         if ($this->isRunningHot()) {
-            return $this->renderDev();
+            return $this->renderDevTags();
         }
 
-        return $this->renderProd();
+        return $this->renderProdTags();
+    }
+
+    /**
+     * Determine if the HMR server is running.
+     */
+    protected function renderDevTags()
+    {
+        //
+    }
+
+    /**
+     * Determine if the HMR server is running.
+     */
+    protected function renderProdTags()
+    {
+        //
+    }
+
+    /**
+     * Determine if the HMR server is running.
+     */
+    protected function isRunningHot(): bool
+    {
+        return file_exists(base_path("packages/{$this->package}/hot"));
     }
 }
