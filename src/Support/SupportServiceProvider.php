2c2adde5d67c3a3037feac19d18bd9accdbeb91d
Ahsan Abrar <iahsanabrar@gmail.com>
2025-11-23 12:56:36 +0500

    Create SupportServiceProvider.php

---
+++ src/Support/SupportServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Spanvel\Support;
+
+use Illuminate\Support\ServiceProvider;
+
+class SupportServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
