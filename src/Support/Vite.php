d89c4351a2d875f6a066bac7e51cd82f9a203c1b
Ahsan Abrar <iahsanabrar@gmail.com>
2025-11-23 15:14:29 +0500

    Update Vite.php

--- src/Support/Vite.php
+++ src/Support/Vite.php
@@ -9,16 +9,6 @@ use Illuminate\Support\HtmlString;
 class Vite
 {
     /**
-     * The name of the package.
-     */
-    protected string $package;
-
-    /**
-     * The main entry file to be processed by Vite.
-     */
-    protected string $entry = 'main.ts';
-
-    /**
      * Handle the invocation of the class.
      */
     public function __invoke(string $package, string $entry = 'main.ts'): HtmlString
